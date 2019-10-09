90ef484e18b0c6374dc7430af5e9bca4cd3aba54
Kenneth Wong <kwong23@asu.edu>
2019-10-08 20:48:39 -0700

    Javadoc commentation of the AddingMachine.java file.

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,75 @@
+//Kenneth Wong
+//ID: 1212586360
+//Assign 2
+package cse360assign2;
+
+/**
+ * 
+ * @Author Kenneth Wong
+ * 
+ * This class contains the methods and a constructor that are used
+ * for a simple calculator that can add and subtract two numbers
+ * and output a result.
+ *
+ */
+
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * This is a constructor used for initializing the variable total.
+	 */
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This is a method used to return the current total of all actions
+	 * performed.
+	 * @return	Returns a value to be printed
+	 */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This method is used to add the total and the given number from
+	 * the parameter value.
+	 * @param value	This parameter is a value given by the user to add to the total.
+	 */
+	
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * The subtract method is used to subtract the parameter from the total.
+	 * @param value	Parameter value is given by user to subtract from total.
+	 */
+	
+	public void subtract (int value) {
+		
+	}
+		
+	/**
+	 * The method toString records all actions done by the user and
+	 * returns the text for all actions and numbers done in the calculator.
+	 * @return returns the string that contains operations and numbers from actions by user.
+	 */
+	
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * This method clears all data from the program by resetting the getTotal to 0
+	 * and by clearing toString so that has no operations or numbers recorded.
+	 */
+	
+	public void clear() {
+	
+	}
+}
